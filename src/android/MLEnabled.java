c69019cd7a2a26c5d401ea334ca5a6c4308aadd6
OscaiitoBM <oherrera@bluemessaging.com>
2017-09-13 13:29:33 -0500

    BMF-2400
    Se añade verificacion de ubicacion encendidad para api level 14-23
    Se remueve verificacion de ubicaciones registradas en un radio de 100m,
    ya que esta situación puede ocurrir aún cuando no se este usando emulador de ubicaciones.
    Se añade verificacion de ubicaciones existentes, esto es, que exista al menos una ubicacion
    registrada por android, de lo contrario se expone a que la ubicacion que cordova registra
    sea insertada por una aplicacion de ubicaciones falsas.
    El mensaje que reporta el plugin tiene la siguiente estructura
    value:msj
    donde value puede ser:
    true -> se esta usando un emulador de ubicaciones
    false -> no esta usando un emulador de ubicaciones (esto no garantiza que la ubicacion obtenida sea real, ya que puede ser un registro 'pegado' de un emulador, pero esta situacion se deja al plugin que se este usando para obtener la ubicacion)
    off -> la ubicacion esta apagada
    nolocation -> Android no tiene registrado ninguna ubicación, por falta de señal, o no ay servicio de red, o alguna otra razon, por lo que es una vulnerabilidad que puede ser explotada para insertar ubicaciones falsas,
    	por lo que esta opcion debe ser manejada con precaución.
    msj -> Es información acerca de la ejecución realizada por el plugin, contiene la version del api level en que se esta trabajando e información acerca de los provedores de de ubicaion e informacion de la ubicacion registrada(si existe).

--- src/android/MLEnabled.java
+++ src/android/MLEnabled.java
@@ -4,82 +4,97 @@ import java.util.List;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CordovaInterface;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
 
 import android.content.Context;
-import android.location.Location; 
+import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.provider.Settings;
-import android.os.Bundle;
+import android.text.TextUtils;
 
 public class MLEnabled extends CordovaPlugin {
-  @Override
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("check")) {      
-      this.check(callbackContext);
-      return true;
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("check")) {
+            this.check(callbackContext);
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
-  private void check(CallbackContext callbackContext) {
-    Context context = this.cordova.getActivity().getApplicationContext();    
-    Location location; 
-    LocationManager locationManager;  
-    String msj = "";
-    boolean anyMock = false;
-    int avalible = 0;
-    try {
-      locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);      
-      if (android.os.Build.VERSION.SDK_INT >= 18) {
-          List<String> providers = locationManager.getProviders(true);          
-          Location locations[] = new Location[providers.size()];  
-          for(String p: providers){
-              msj += "\n\nProvider= " + p;
-              LocationProvider lp = locationManager.getProvider(p);
-              msj += "\naccuracy= " + lp.getAccuracy();
-              msj += "\nrequireNetwork= " + lp.requiresNetwork();
-              msj += "\nreqquireSatelite= " + lp.requiresSatellite();
-              msj += "\nsupportAltitude= " + lp.supportsAltitude();
-              msj += "\nsupportBearing= " + lp.supportsBearing();
-              msj += "\nsupportSpeed= " + lp.supportsSpeed();
-              boolean isEnabled = locationManager.isProviderEnabled(lp.getName());
-              msj += "\nisEnabled= " + isEnabled;
-              if(isEnabled){                  
-                  location = locationManager.getLastKnownLocation(p);
-                  if(location != null){
-                      locations[avalible++] = location; 
-                      msj += "\nIsFromMockProvider= " + location.isFromMockProvider();
-                      if(location.isFromMockProvider()) anyMock = true;
-                      msj += "\nlatitude= " + location.getLatitude() + " longitude= " + location.getLongitude();
-                  }                  
-              } 
-          }
-          if(!anyMock){
-              mockFound:
-                  for(int i = 0; i < avalible; i++){
-                      for(int j = 0; j < avalible; j++){
-                          if(locations[i].distanceTo(locations[j]) > 100){
-                              anyMock = true;
-                              break mockFound;
-                          }
-                      }
-                  }
-          }          
-          msj = anyMock + ":" + msj;
-      } else {
-        msj += !Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0");
-      }      
-      callbackContext.success(msj);
-    } catch (Exception e) {            
-      callbackContext.error(e.getMessage());
+
+    private void check(CallbackContext callbackContext) {
+        Context context = this.cordova.getActivity().getApplicationContext();
+        LocationManager locationManager;
+        Location location;
+        boolean anyMock = true, locationOff = false;
+        int avalibleProviders = 0, avalibleLocations = 0;
+        int sdk_int = android.os.Build.VERSION.SDK_INT;
+        String msj = "";
+        try {
+            msj = "\nAPI_LEVEL=" + sdk_int;
+            /* Check if location it's turned on */
+            if (sdk_int < android.os.Build.VERSION_CODES.KITKAT) {
+                String prov = Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+                locationOff = TextUtils.isEmpty(prov);
+                msj += "\nLOCATION_PROVIDERS_ALLOWED=empty";
+            }
+            else {
+                int modeResult = Settings.Secure.getInt(this.cordova.getActivity().getContentResolver(), Settings.Secure.LOCATION_MODE);
+                locationOff = Settings.Secure.LOCATION_MODE_OFF == modeResult;
+                msj += "\nLOCATION_MODE=" + modeResult;
+            }
+            if (locationOff) {
+                callbackContext.success("off:" + msj);
+                return;
+            }
+            locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+            if (android.os.Build.VERSION.SDK_INT >= 18) {// Jelly Bean 4.3.x
+                List<String> providers = locationManager.getProviders(true);
+                Location locations[] = new Location[providers.size()];
+                for (String p : providers) {
+                    msj += "\n\bProvider= " + p;
+                    LocationProvider lp = locationManager.getProvider(p);
+                    msj += "\naccuracy= " + lp.getAccuracy();
+                    msj += "\nrequireNetwork= " + lp.requiresNetwork();
+                    msj += "\nreqquireSatelite= " + lp.requiresSatellite();
+                    msj += "\nsupportAltitude= " + lp.supportsAltitude();
+                    msj += "\nsupportBearing= " + lp.supportsBearing();
+                    msj += "\nsupportSpeed= " + lp.supportsSpeed();
+                    boolean isEnabled = locationManager.isProviderEnabled(lp.getName());
+                    msj += "\nisEnabled= " + isEnabled;
+                    if (isEnabled) {
+                        location = locationManager.getLastKnownLocation(p);
+                        // anyMock &= location != null;
+                        if (location != null) {
+                            locations[avalibleProviders++] = location;
+                            msj += "\nIsFromMockProvider= " + location.isFromMockProvider();
+                            anyMock &= location.isFromMockProvider();
+                            msj += "\nlatitude= " + location.getLatitude() + " longitude= " + location.getLongitude();
+                            avalibleLocations++;
+                        }
+                        else {
+                            msj += "\nNo location";
+                        }
+                    }
+                }
+                if (avalibleLocations == 0) {
+                    msj = "nolocation:" + msj;
+                }
+                else {
+                    msj = anyMock + ":" + msj;
+                }
+            }
+            else {
+                msj = !Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0") + ":" + msj;
+            }
+            callbackContext.success(msj);
+        }
+        catch (Exception e) {
+            callbackContext.error("Exception\n" + e.getMessage());
+        }
     }
-  }
-  
+
 }
